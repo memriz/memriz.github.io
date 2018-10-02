9cc59ef9fcf724b49c95a45430d808660c19e079
Mohit <mohit@izap.in>
2018-10-02 15:55:18 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -139,77 +139,8 @@ jQuery(document).ready(function(jQuery) {
 		});
 	}
 
-	/*------------------------------------------------------------------
-	[ Instagram carousel ]
-	*/
-	
-	var userFeed = new Instafeed({
-		get: 'user',
-		userId: '8583854610',
-		target: 'memrizfeed',
-		clientId: '0261b26d624348e6a4ad35372157b775',
-		accessToken: '8583854610.0261b26.1d34c2e8b8ef48348be37aabfe44eb4c',
-		resolution: 'standard_resolution',
-		template: '<div class="item" style="background-image: url({{image}});"><a href="javascript:void(0);"></a></div>',
-		sortBy: 'most-recent',
-		limit: 10,
-		after: function() {
-			// disable button if no more results to load
-			if(jQuery('.ig-carousel').length > 0){
-			jQuery('.ig-carousel').owlCarousel({
-				loop:true,
-				items:4,
-				nav: false,
-				dots: false,
-				smartSpeed: 5000,
-				autoplay: true,
-				autoplayTimeout: 100,
-				autoplayHoverPause: true,
-				responsive:{
-					0:{
-						items:2
-					},
-					768:{
-						items:2
-					},
-					990:{
-						items:3
-					},
-					1024:{
-						items:4
-					}
-				}
-			});
-		}
-	},
-	});
-	userFeed.run();
-	// if(jQuery('.ig-carousel').length > 0){
-	// 	jQuery('.ig-carousel').owlCarousel({
-	// 		loop:true,
-	// 		items:4,
-	// 		nav: false,
-	// 		dots: false,
-	// 		smartSpeed: 5000,
-	// 		autoplay: true,
-	// 		autoplayTimeout: 100,
-	// 		autoplayHoverPause: true,
-	// 		responsive:{
-	// 			0:{
-	// 				items:2
-	// 			},
-	// 			768:{
-	// 				items:2
-	// 			},
-	// 			990:{
-	// 				items:3
-	// 			},
-	// 			1024:{
-	// 				items:4
-	// 			}
-	// 		}
-	// 	});
-	// }
+
+
 	
 	/*------------------------------------------------------------------
 	[ Info section ]
@@ -416,6 +347,52 @@ jQuery(document).ready(function(jQuery) {
 	    });
 	}
 
+		/*------------------------------------------------------------------
+	[ Instagram carousel ]
+	*/
+	
+	var userFeed = new Instafeed({
+		get: 'user',
+		userId: '8583854610',
+		target: 'memrizfeed',
+		clientId: '0261b26d624348e6a4ad35372157b775',
+		accessToken: '8583854610.0261b26.1d34c2e8b8ef48348be37aabfe44eb4c',
+		resolution: 'standard_resolution',
+		template: '<div class="item" style="background-image: url({{image}});"><a href="javascript:void(0);"></a></div>',
+		sortBy: 'most-recent',
+		limit: 10,
+		after: function() {
+			// disable button if no more results to load
+			if(jQuery('.ig-carousel').length > 0){
+			jQuery('.ig-carousel').owlCarousel({
+				loop:true,
+				items:4,
+				nav: false,
+				dots: false,
+				smartSpeed: 5000,
+				autoplay: true,
+				autoplayTimeout: 100,
+				autoplayHoverPause: true,
+				responsive:{
+					0:{
+						items:2
+					},
+					768:{
+						items:2
+					},
+					990:{
+						items:3
+					},
+					1024:{
+						items:4
+					}
+				}
+			});
+		}
+	},
+	});
+	userFeed.run();
+	
 	/*------------------------------------------------------------------
 	[ Animation ]
 	*/
@@ -442,3 +419,32 @@ jQuery(document).ready(function(jQuery) {
 		});
 	});
 });
+
+
+// script modal
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
